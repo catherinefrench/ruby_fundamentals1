2c77dbae5c686a11270ec984d7cff10dbceec4b1
Catherine French <catherine.french@me.com>
2016-08-09 15:41:17 -0400

    Asks user for age, interprets age as interger, caculates user birth year, putses birth year to user.

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
-puts "What is your name?"
-name = gets
-puts "Greetings #{name}!"
+puts "What is your name?" #Asks user for their name
+name = gets #Assigns name to variable
+puts "Greetings #{name}" #Greets user by name
+
+puts "How old are you?" #Asks user for age
+age = gets.to_i #Assigns age to variable
+year = 2016 #Assigns the current year to variable
+birth_year = year - age #caculates user's birth year based on inputs
+puts "If you are #{age} then you were born in #{birth_year}!" #putses birthyear to user
