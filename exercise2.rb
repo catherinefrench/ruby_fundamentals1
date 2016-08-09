b6eb078a0269e09c9df4853919b6708df1d773a1
Catherine French <catherine.french@me.com>
2016-08-09 15:14:23 -0400

    Calculated tip on meal, added strings together, putsed the product of two numbers, putsed the value of an expression

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+meal = 55 # Assigns variable meal to cost of meal
+tip = 0.15 # Assigns variable tip to desired tip percentage
+cost = meal * tip + meal #calculates cost of meal based on meal and tip values
+puts "The total cost of your meal is #{cost}" #outputs total cost of meal
+
+puts "Hello world" + " 10"
+
+product = 45628 * 7839
+puts "The product of 45628 and 7839 is #{product}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
