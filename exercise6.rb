e5aac203c5c930741129f6257ee0861565be8bce
Catherine French <catherine.french@me.com>
2016-08-10 15:59:28 -0400

    Created a method to iterate over grocery list; putsed list with aestrix prefacing each line item.

---
+++ exercise6.rb
@@ -0,0 +1,7 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list.each do |list| #creates a method to iterate over the list
+
+  puts "* #{list}" #putses list with an asterix to begin each line
+
+end
