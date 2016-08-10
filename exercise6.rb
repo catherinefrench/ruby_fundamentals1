2f59449aa4fc4325eae3b769ae42058944416477
Catherine French <catherine.french@me.com>
2016-08-10 16:46:21 -0400

    Checked whether list included bananas then putsed second item in list.

--- exercise6.rb
+++ exercise6.rb
@@ -1,10 +1,20 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list << "rice" #adds rice to list 
+grocery_list << "rice" #adds rice to list
 
 grocery_list.each do |list| #creates a method to iterate over the list
 
-
   puts "* #{list}" #putses list with an asterix to begin each line
-
 end
+
+puts "You have #{grocery_list.count} items on your list." #putses number of items in list
+
+if grocery_list.include? ("bananas") #conditional putsing whether or not you need bananas.
+    puts "You don't need more bananas."
+  else
+    puts "Don't forget to pick up bananas."
+end
+
+puts "The second item on your list is #{grocery_list[1]}." #Displays second item on list
+
+puts grocery_list.sort
