d8753306629fe6107816418b4d4f7184488c9ea5
Catherine French <catherine.french@me.com>
2016-08-10 16:01:24 -0400

    Added rice to the list.

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,9 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+grocery_list << "rice" #adds rice to list 
+
 grocery_list.each do |list| #creates a method to iterate over the list
+
 
   puts "* #{list}" #putses list with an asterix to begin each line
 
